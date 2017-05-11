732a4a67b375cffe73f2692b6931c563adc2a00d
Derik Breedt <dbreedt@retrorabbit.co.za>
2017-05-11 16:58:03 +0200

    Added support for kw4

--- gener8.go
+++ gener8.go
@@ -1,98 +1,106 @@
 package main
 
 import (
-  "flag"
-  "io/ioutil"
-  "os"
-  "path/filepath"
-  "fmt"
-  "regexp"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
 )
 
 type gener8 struct {
-  in string
-  out string
-  pkg string
-  kw1 string
-  kw2 string
-  kw3 string
-  inData []byte
+	in     string
+	out    string
+	pkg    string
+	kw1    string
+	kw2    string
+	kw3    string
+	kw4    string
+	inData []byte
 }
 
 func (g *gener8) generate() {
-  g.init()
+	g.init()
 
-  outData := string(g.inData)
+	outData := string(g.inData)
 
-  if g.pkg != "" {
-    rxPkg := regexp.MustCompile(`\$pkg`)
+	if g.pkg != "" {
+		rxPkg := regexp.MustCompile(`\$pkg`)
 
-    outData = rxPkg.ReplaceAllString(outData, g.pkg)
-  }
+		outData = rxPkg.ReplaceAllString(outData, g.pkg)
+	}
 
-  if g.kw1 != "" {
-    rxKw1 := regexp.MustCompile(`\$kw1`)
+	if g.kw1 != "" {
+		rxKw1 := regexp.MustCompile(`\$kw1`)
 
-    outData = rxKw1.ReplaceAllString(outData, g.kw1)
-  }
+		outData = rxKw1.ReplaceAllString(outData, g.kw1)
+	}
 
-  if g.kw2 != "" {
-    rxKw2 := regexp.MustCompile(`\$kw2`)
+	if g.kw2 != "" {
+		rxKw2 := regexp.MustCompile(`\$kw2`)
 
-    outData = rxKw2.ReplaceAllString(outData, g.kw2)
-  }
+		outData = rxKw2.ReplaceAllString(outData, g.kw2)
+	}
 
-  if g.kw3 != "" {
-    rxKw3 := regexp.MustCompile(`\$kw3`)
+	if g.kw3 != "" {
+		rxKw3 := regexp.MustCompile(`\$kw3`)
 
-    outData = rxKw3.ReplaceAllString(outData, g.kw3)
-  }
+		outData = rxKw3.ReplaceAllString(outData, g.kw3)
+	}
 
-  pwd, err := os.Getwd()
+	if g.kw4 != "" {
+		rxKw4 := regexp.MustCompile(`\$kw4`)
 
-  check(err)
+		outData = rxKw4.ReplaceAllString(outData, g.kw4)
+	}
 
-  path := filepath.Join(pwd, g.out)
+	pwd, err := os.Getwd()
 
-  err = ioutil.WriteFile(path, []byte(outData), 0644) // r.w r.. r..
+	check(err)
 
-  check(err)
+	path := filepath.Join(pwd, g.out)
+
+	err = ioutil.WriteFile(path, []byte(outData), 0644) // r.w r.. r..
+
+	check(err)
 }
 
 func (g *gener8) init() {
-  flag.StringVar(&g.in, "in", "", "file to parse")
-  flag.StringVar(&g.out, "out", "", "file to write the generated code to")
-  flag.StringVar(&g.pkg, "pkg", "", "the value to replace $pkg with")
-  flag.StringVar(&g.kw1, "kw1", "", "the value to replace $kw1 with")
-  flag.StringVar(&g.kw2, "kw2", "", "the value to replace $kw2 with")
-  flag.StringVar(&g.kw3, "kw3", "", "the value to replace $kw3 with")
+	flag.StringVar(&g.in, "in", "", "file to parse")
+	flag.StringVar(&g.out, "out", "", "file to write the generated code to")
+	flag.StringVar(&g.pkg, "pkg", "", "the value to replace $pkg with")
+	flag.StringVar(&g.kw1, "kw1", "", "the value to replace $kw1 with")
+	flag.StringVar(&g.kw2, "kw2", "", "the value to replace $kw2 with")
+	flag.StringVar(&g.kw3, "kw3", "", "the value to replace $kw3 with")
+	flag.StringVar(&g.kw4, "kw4", "", "the value to replace $kw4 with")
 
-  flag.Parse()
+	flag.Parse()
 
-  if g.in == "" || g.out == "" {
-    fmt.Printf("in and out are required\n")
-    os.Exit(1)
-  }
+	if g.in == "" || g.out == "" {
+		fmt.Printf("in and out are required\n")
+		os.Exit(1)
+	}
 
-  pwd, err := os.Getwd()
+	pwd, err := os.Getwd()
 
-  check(err)
+	check(err)
 
-  path := filepath.Join(pwd, g.in)
+	path := filepath.Join(pwd, g.in)
 
-  g.inData, err = ioutil.ReadFile(path)
+	g.inData, err = ioutil.ReadFile(path)
 
-  check(err)
+	check(err)
 }
 
 func check(err error) {
-  if err != nil {
-    fmt.Printf("%+v\n", err)
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(1)
+	}
 }
 
 func main() {
-  g:= &gener8{}
-  g.generate()
+	g := &gener8{}
+	g.generate()
 }
