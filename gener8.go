91e78d70c4c32b9788d29a13eb09492a2a4b40ca
Derik Breedt <dbreedt@retrorabbit.co.za>
2018-11-08 11:48:01 +0200

    Added tracing and better error reporting arround gofmt

--- gener8.go
+++ gener8.go
@@ -20,47 +20,76 @@ type gener8 struct {
 	pkg        string
 	kws        string
 	inData     []byte
+	trace      bool
 }
 
 func (g *gener8) generate() {
 	outData := string(g.inData)
 
 	if g.pkg != "" {
+		g.traceOut("generate:Parsing pkg...")
+
 		rxPkg := regexp.MustCompile(`\$pkg`)
 
 		outData = rxPkg.ReplaceAllString(outData, g.pkg)
+
+		g.traceOut("generate:Parsing pkg complete")
 	}
 
 	if g.kws != "" {
+		g.traceOut("generate:Parsing kws...")
+
 		keywords, err := parseKws(g.kws)
 
-		if err != nil {
-			panic(err)
-		}
+		check(err)
+
+		g.traceOut("generate:Parsing pkg complete")
 
 		for i := len(*keywords) - 1; i > -1; i-- {
+			g.traceOut("generate:processing $kw%d", i+1)
+
 			kw := (*keywords)[i]
 			rxKw := regexp.MustCompile(fmt.Sprintf("\\$kw%d", i+1))
 			outData = rxKw.ReplaceAllString(outData, kw)
+
+			g.traceOut("generate:processing $kw%d complete", i+1)
 		}
 	}
+
+	g.traceOut("generate:Getwd")
 
 	pwd, err := os.Getwd()
 
 	check(err)
 
+	g.traceOut("generate:Construct out path")
+
 	path := filepath.Join(pwd, g.out)
 
 	outData = fmt.Sprintf("// %s Auto generated from %s by gener8\n\n", time.Now(), g.in) + outData
+
+	g.traceOut("generate:WriteFile: path: %s", path)
 
 	err = ioutil.WriteFile(path, []byte(outData), 0644) // r.w r.. r..
 
 	check(err)
 
+	g.traceOut("generate:WriteFile Complete")
+
 	if !g.skipFormat {
+		g.traceOut("generate:Formatting output file")
+
 		cmd := exec.Command("gofmt", "-w", path)
+		cmd.Stderr = os.Stderr
 		err = cmd.Run()
-		check(err)
+
+		if err != nil {
+			check(fmt.Errorf("gofmt -w %s failed: %s", g.out, err))
+		}
+
+		g.traceOut("generate:Formatting complete")
+	} else {
+		g.traceOut("generate:SkipFormat")
 	}
 }
 
@@ -76,6 +105,7 @@ func parseKws(kws string) (*[]string, error) {
 
 func (g *gener8) setup() {
 	flag.BoolVar(&g.skipFormat, "skip_format", false, "skip gofmt being run on the generated file")
+	flag.BoolVar(&g.trace, "trace", false, "enables trace logging")
 	flag.StringVar(&g.in, "in", "", "file to parse")
 	flag.StringVar(&g.out, "out", "", "file to write the generated code to")
 	flag.StringVar(&g.pkg, "pkg", "", "the value to replace $pkg with")
@@ -88,11 +118,15 @@ func (g *gener8) setup() {
 		os.Exit(1)
 	}
 
+	g.traceOut("setup:Complete flag.Parse")
+
 	pwd, err := os.Getwd()
 
 	check(err)
 
 	path := filepath.Join(pwd, g.in)
+
+	g.traceOut("setup:ReadFile: %s", path)
 
 	g.inData, err = ioutil.ReadFile(path)
 
@@ -101,8 +135,14 @@ func (g *gener8) setup() {
 
 func check(err error) {
 	if err != nil {
-		fmt.Printf("%+v\n", err)
+		fmt.Fprintf(os.Stderr, "%+v\n", err)
 		os.Exit(1)
+	}
+}
+
+func (g *gener8) traceOut(format string, a ...interface{}) {
+	if g.trace {
+		fmt.Fprintf(os.Stderr, format+"\n", a...)
 	}
 }
 
